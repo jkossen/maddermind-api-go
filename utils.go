19fdad070305fd2540ac3df2423158434ea5a5f3
Jochem Kossen <jochem@jkossen.nl>
2022-03-02 21:36:53 +0100

    Pass time as argument so the function is testable, add some documentation

--- utils.go
+++ utils.go
@@ -33,8 +33,9 @@ func RandString(n int) string {
 	return string(dstChars)
 }
 
-func StartOfDayEpoch() int64 {
-	t := time.Now()
+// StartOfDayEpoch determines the Unix timestamp in seconds for 00:00am this day
+// It returns the resulting int64
+func StartOfDayEpoch(t time.Time) int64 {
 	year, month, day := t.Year(), t.Month(), t.Day()
 	tt, _ := time.Parse(time.RFC3339, fmt.Sprintf("%d-%02d-%02dT00:00:00+00:00", year, month, day))
 	return tt.Unix()
