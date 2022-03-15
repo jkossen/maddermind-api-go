6d95c7f4526b87a9019f29ecbd8d5df4ea82ec5a
Jochem Kossen <jochem@jkossen.nl>
2022-03-15 23:00:15 +0100

    Add test for SepEveryNth

---
+++ strutil/string_test.go
@@ -0,0 +1,31 @@
+package strutil
+
+import (
+	"testing"
+)
+
+type testSet struct {
+	in, want, sep string
+	n             int
+}
+
+func TestSepEveryNth(t *testing.T) {
+	sets := []testSet{
+		testSet{"aaaaaaaaaaa", "aaaa-aaaa-aaa", "-", 4},
+		testSet{"aaaaaaaaaaaa", "aaaa-aaaa-aaaa", "-", 4},
+		testSet{"aaaaaaaa", "aaaa-aaaa", "-", 4},
+		testSet{"aaaa", "aaaa", "-", 4},
+		testSet{"aaa", "aaa", "-", 3},
+		testSet{"aaaaaaaaaaa", "a-a-a-a-a-a-a-a-a-a-a", "-", 1},
+		testSet{"aaaaaaaaaaa", "aaa-aaa-aaa-aa", "-", 3},
+		testSet{"-----", "------", "-", 4},
+	}
+
+	for _, row := range sets {
+		var res = SepEveryNth(row.in, row.n, row.sep)
+
+		if res != row.want {
+			t.Fatalf(`SepEveryNth(%v, %v, %v): %v != %v`, row.in, row.n, row.sep, res, row.want)
+		}
+	}
+}
